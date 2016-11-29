488e756cae424fab202b546bce5d6b43e3932c4e
beata <beata.baran7.03@gmail.com>
2016-11-29 23:15:45 +0100

    new module Home, refactoring

--- Home/src/Controller.java
+++ Home/src/Controller.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
--- User/src/model/User.java
+++ User/src/model/User.java
@@ -1,4 +1,4 @@
-package sample.model;
+package model;
 
 
 public class User {
