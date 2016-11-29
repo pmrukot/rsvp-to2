56b4d9587c01f1e1ab4882fbd3547c5a1e37350d
apiechowicz <andrzej.piechowicz.93@gmail.com>
2016-11-29 23:10:24 +0100

    added new fields (firstname, lastname, isadmin), generated constructor, getters and setters

--- User/src/sample/model/User.java
+++ User/src/sample/model/User.java
@@ -1,8 +1,57 @@
 package sample.model;
 
-
 public class User {
+    public User(String login, String firstName, String lastName, String password, boolean isAdmin) {
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
 
     private String login;
+    private String firstName;
+    private String lastName;
     private String password;
+    private boolean isAdmin;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
