339bbc09aacee856ff1e083e1daeb6e2a8fef915
beata <beata.baran7.03@gmail.com>
2016-11-30 00:29:30 +0100

    my calendar tab

--- Home/src/Controller.java
+++ Home/src/Controller.java
@@ -1,5 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
@@ -7,9 +8,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
     public TextField password;
@@ -21,20 +27,50 @@ public class Controller {
             Stage homeStage = new Stage();
             Group root = new Group();
             homeStage.setTitle("Booking rooms");
-            Scene scene = new Scene(root, 600, 500);
+            Scene scene = new Scene(root, 800, 600);
 
             TabPane tabPane = new TabPane();
 
             BorderPane borderPane = new BorderPane();
-            for (int i = 0; i < 5; i++) {
+
+            Tab myCalendarTab = new Tab();
+            myCalendarTab.setText("My calendar");
+
+
+            Label label = new Label("My Calendar");
+            label.setFont(new Font("Arial", 20));
+
+
+            TableView table = new TableView();
+            table.setEditable(false);
+            table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+            List<TableColumn> columns = new ArrayList();
+            for(int i = 1; i<=7 ; i++) {
+                columns.add(new TableColumn(DayOfWeek.of(i).name()));
+            }
+
+            table.getColumns().addAll(columns);
+
+            VBox vBox = new VBox();
+            vBox.setSpacing(5);
+            vBox.setPadding(new Insets(0, 10, 10, 10));
+            vBox.getChildren().addAll(label, table);
+
+            vBox.setAlignment(Pos.CENTER);
+
+            myCalendarTab.setContent(vBox);
+            tabPane.getTabs().add(myCalendarTab);
+
+            for (int i = 0; i < 2; i++) {
                 Tab tab = new Tab();
                 tab.setText("Tab" + i);
-                HBox hbox = new HBox();
-                hbox.getChildren().add(new Label("Tab" + i));
-                hbox.setAlignment(Pos.CENTER);
-                tab.setContent(hbox);
+                HBox h = new HBox();
+                h.getChildren().add(new Label("Tab" + i));
+                h.setAlignment(Pos.CENTER);
+                tab.setContent(h);
                 tabPane.getTabs().add(tab);
             }
+
             // bind to take available space
             borderPane.prefHeightProperty().bind(scene.heightProperty());
             borderPane.prefWidthProperty().bind(scene.widthProperty());
@@ -42,8 +78,6 @@ public class Controller {
             borderPane.setCenter(tabPane);
             root.getChildren().add(borderPane);
             homeStage.setScene(scene);
-
-
 
             homeStage.show();
         }
