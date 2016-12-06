76921386974c52aa8bd6ef94de2ad576b1081ea0
apiechowicz <andrzej.piechowicz.93@gmail.com>
2016-12-07 00:07:29 +0100

    AdminView: added 3x button (create, edit, delete) and combobox for users

--- src/rsvp/user/controller/AdminController.java
+++ src/rsvp/user/controller/AdminController.java
@@ -1,14 +1,19 @@
 package rsvp.user.controller;
 
-
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import rsvp.user.model.DBUserDAO;
+import rsvp.user.model.User;
+import rsvp.user.model.UserDAO;
 import rsvp.user.model.UserUtils;
-
 import java.io.File;
 
 public class AdminController {
+    @FXML
+    private ComboBox<User> userComboBox;
 
     public void upload(ActionEvent actionEvent) {
         FileChooser chooser = new FileChooser();
@@ -17,5 +22,26 @@ public class AdminController {
         chooser.getExtensionFilters().add(extFilter);
         File file = chooser.showOpenDialog(new Stage());
         UserUtils.createUsersFromCsv(file);
+    }
+
+    public void createSingleUSer(ActionEvent actionEvent) {
+        System.out.println("implement me");
+    }
+
+    public void editUser(ActionEvent actionEvent) {
+        User selected = userComboBox.getSelectionModel().getSelectedItem();
+        System.out.println("Selected user: " + selected.getLogin());
+    }
+
+    public void deleteUser(ActionEvent actionEvent) {
+        User selected = userComboBox.getSelectionModel().getSelectedItem();
+        System.out.println("Selected user: " + selected.getLogin());
+    }
+
+    public void loadUsers() {
+        userComboBox.getItems().clear();
+        UserDAO userDAO = new DBUserDAO();
+        userComboBox.getItems().addAll(userDAO.findUsersByName(""));
+        //return userDAO.findUsersByName("");
     }
 }
