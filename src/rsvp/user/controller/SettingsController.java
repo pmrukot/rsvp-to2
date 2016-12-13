643719b1d8dccc5211b1fff8f074f5d9809d5cef
beata <beata.baran7.03@gmail.com>
2016-12-13 19:37:03 +0100

    fixed button binding in settings tab, SettingsController decomposition

--- src/rsvp/user/controller/SettingsController.java
+++ src/rsvp/user/controller/SettingsController.java
@@ -44,13 +44,8 @@ public class SettingsController implements Initializable{
         login.setText(currentUser.getLogin());
         firstName.setText(currentUser.getFirstName());
         lastName.setText(currentUser.getLastName());
-        BooleanBinding changePasswordBinding = oldPassword.textProperty().isEmpty().or(newPassword.textProperty().isEmpty())
-                .or(newPassword2.textProperty().isEmpty());
-        changePasswordButton.disableProperty().bind(changePasswordBinding);
-        BooleanBinding changeUserDataBinding = login.textProperty().isEqualTo(currentUser.getLogin())
-                .and(firstName.textProperty().isEqualTo(currentUser.getFirstName()))
-                .and(lastName.textProperty().isEqualTo(currentUser.getLastName()));
-        changeUserDataButton.disableProperty().bind(changeUserDataBinding);
+        addChangePasswordBinding();
+        addChangeUserDataBinding(currentUser);
     }
 
     public void changePassword() {
@@ -61,38 +56,21 @@ public class SettingsController implements Initializable{
                     UserDAO userDAO = new DBUserDAO();
                     currentUser.setPassword(newPassword.getText());
                     if(userDAO.updateUser(currentUser)) {
-                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                        alert.setTitle("Information Dialog");
-                        alert.setHeaderText("Password changed successfully!");
-                        alert.showAndWait();
-                        oldPassword.setText("");
-                        newPassword.setText("");
-                        newPassword2.setText("");
+                        showInformationDialog("Password changed successfully!");
+                        clearPasswordTextFields();
                         instance.refreshUsers();
                     } else {
                         currentUser.setPassword(oldPassword.getText());
-                        Alert alert = new Alert(Alert.AlertType.ERROR);
-                        alert.setTitle("Error Dialog");
-                        alert.setHeaderText("Failed to change password!");
-                        alert.showAndWait();
+                        showErrorDialog("Failed to change password!");
                     }
                 } else {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error Dialog");
-                    alert.setHeaderText("Passwords do not match!");
-                    alert.showAndWait();
+                    showErrorDialog("Passwords do not match!");
                 }
             } else {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error Dialog");
-                alert.setHeaderText("New password must be different from current!");
-                alert.showAndWait();
+                showErrorDialog("New password must be different from current!");
             }
         } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error Dialog");
-            alert.setHeaderText("Current password is incorrect!");
-            alert.showAndWait();
+            showErrorDialog("Current password is incorrect!");
         }
     }
 
@@ -107,19 +85,51 @@ public class SettingsController implements Initializable{
         currentUser.setLastName(lastName.getText());
 
         if(userDAO.updateUser(currentUser)) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Information Dialog");
-            alert.setHeaderText("User data changed successfully!");
-            alert.showAndWait();
+            showInformationDialog("User data changed successfully!");
             instance.refreshUsers();
+            addChangeUserDataBinding(currentUser);
         } else {
-            currentUser.setLogin(backupLogin);
-            currentUser.setFirstName(backupFirstName);
-            currentUser.setLastName(backupLastName);
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error Dialog");
-            alert.setHeaderText("Failed to change user data!");
-            alert.showAndWait();
+            backupUser(backupLogin, backupFirstName, backupLastName, currentUser);
+            showErrorDialog("Failed to change user data!");
         }
+    }
+
+    private void addChangeUserDataBinding(User currentUser) {
+        BooleanBinding changeUserDataBinding = login.textProperty().isEqualTo(currentUser.getLogin())
+                .and(firstName.textProperty().isEqualTo(currentUser.getFirstName()))
+                .and(lastName.textProperty().isEqualTo(currentUser.getLastName()));
+        changeUserDataButton.disableProperty().bind(changeUserDataBinding);
+    }
+
+    private void addChangePasswordBinding() {
+        BooleanBinding changePasswordBinding = oldPassword.textProperty().isEmpty().or(newPassword.textProperty().isEmpty())
+                .or(newPassword2.textProperty().isEmpty());
+        changePasswordButton.disableProperty().bind(changePasswordBinding);
+    }
+
+    private void showErrorDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error Dialog");
+        alert.setHeaderText(message);
+        alert.showAndWait();
+    }
+
+    private void showInformationDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(message);
+        alert.showAndWait();
+    }
+
+    private void clearPasswordTextFields() {
+        oldPassword.setText("");
+        newPassword.setText("");
+        newPassword2.setText("");
+    }
+
+    private void backupUser(String login, String firstName, String lastName, User currentUser) {
+        currentUser.setLogin(login);
+        currentUser.setFirstName(firstName);
+        currentUser.setLastName(lastName);
     }
 }
