a40b9070e1534ad81979690026023854761f1bc3
apiechowicz <andrzej.piechowicz.93@gmail.com>
2017-01-26 19:43:21 +0100

    changed property names to better reflect their meanings. kind of fixed tests.

--- test/rsvp/user/command/CommandManagerTest.java
+++ test/rsvp/user/command/CommandManagerTest.java
@@ -1,15 +1,10 @@
 package rsvp.user.command;
 
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import rsvp.user.DAO.DBUserDAO;
-import rsvp.user.DAO.UserDAO;
-import rsvp.user.model.User;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -20,62 +15,53 @@ import static org.mockito.Mockito.when;
 public class CommandManagerTest {
 
     @Mock
-    private UserDAO userDAO;
+    private Command command;
 
     @InjectMocks
     private CommandManager commandManager;
 
-
     @Test
     public void testExecuteCommand() {
-        User u  = new User("Jan", "Nowak", "pass", true);
-        Command c = new CreateUserCommand(userDAO, u);
-        when(c.execute()).thenReturn(true);
-
-        boolean result = commandManager.executeCommand(c);
-
+        when(command.execute()).thenReturn(true);
+        boolean result = commandManager.executeCommand(command);
         assertTrue(result);
-        assertThat(commandManager.undoPossible.get(), is(false));
-        assertThat(commandManager.redoPossible.get(), is(true));
+        assertThat(commandManager.undoImpossible.get(), is(false));
+        assertThat(commandManager.redoImpossible.get(), is(true));
         assertThat(commandManager.getExecutedCommandsStackSize(), is(1));
-        assertThat(commandManager.getExecutedCommands().firstElement(), is(c));
+        assertThat(commandManager.getExecutedCommands().firstElement(), is(command));
         assertThat(commandManager.getRedoableCommandsStackSize(), is(0));
     }
 
     @Test
     public void testExecuteCommandThanRevert() {
-        User u  = new User("Jan", "Nowak", "pass", true);
-        Command c = new CreateUserCommand(userDAO, u);
-        when(c.execute()).thenReturn(true);
-        when(c.undo()).thenReturn(true);
+        when(command.execute()).thenReturn(true);
+        when(command.undo()).thenReturn(true);
 
-        commandManager.executeCommand(c);
+        commandManager.executeCommand(command);
         commandManager.undo();
 
-        assertThat(commandManager.undoPossible.get(), is(true));
-        assertThat(commandManager.redoPossible.get(), is(false));
+        assertThat(commandManager.undoImpossible.get(), is(true));
+        assertThat(commandManager.redoImpossible.get(), is(false));
         assertThat(commandManager.getExecutedCommandsStackSize(), is(0));
-        assertThat(commandManager.getRedoableCommands().firstElement(), is(c));
+        assertThat(commandManager.getRedoableCommands().firstElement(), is(command));
         assertThat(commandManager.getRedoableCommandsStackSize(), is(1));
     }
 
     @Test
     public void testRedoCommand() {
-        User u  = new User("Jan", "Nowak", "pass", true);
-        Command c = new CreateUserCommand(userDAO, u);
-        when(c.execute()).thenReturn(true);
-        when(c.undo()).thenReturn(true);
-        when(c.redo()).thenReturn(true);
+        when(command.execute()).thenReturn(true);
+        when(command.undo()).thenReturn(true);
+        when(command.redo()).thenReturn(true);
 
-        boolean result = commandManager.executeCommand(c);
+        boolean result = commandManager.executeCommand(command);
         commandManager.undo();
         commandManager.redo();
 
         assertTrue(result);
-        assertThat(commandManager.undoPossible.get(), is(false));
-        assertThat(commandManager.redoPossible.get(), is(true));
+        assertThat(commandManager.undoImpossible.get(), is(false));
+        assertThat(commandManager.redoImpossible.get(), is(true));
         assertThat(commandManager.getExecutedCommandsStackSize(), is(1));
-        assertThat(commandManager.getExecutedCommands().firstElement(), is(c));
+        assertThat(commandManager.getExecutedCommands().firstElement(), is(command));
         assertThat(commandManager.getRedoableCommandsStackSize(), is(0));
     }
 }
--- test/rsvp/user/command/CreateUserCommandTest.java
+++ test/rsvp/user/command/CreateUserCommandTest.java
@@ -1,19 +1,57 @@
 package rsvp.user.command;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import rsvp.user.DAO.UserDAO;
+import rsvp.user.model.User;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CreateUserCommandTest {
-    @org.junit.Test
+
+    @Mock
+    private UserDAO userDAO;
+
+    @Mock
+    private User user;
+
+    @InjectMocks
+    private CreateUserCommand command;
+
+    @Test
     public void execute() throws Exception {
-
+        when(userDAO.createUser(user)).thenReturn(true);
+        boolean result = command.execute();
+        assertTrue(result);
+        when(userDAO.createUser(user)).thenReturn(false);
+        boolean result2 = command.execute();
+        assertFalse(result2);
     }
 
-    @org.junit.Test
+    @Test
     public void undo() throws Exception {
-
+        when(userDAO.deleteUser(user)).thenReturn(true);
+        boolean result = command.undo();
+        assertTrue(result);
+        when(userDAO.deleteUser(user)).thenReturn(false);
+        boolean result2 = command.undo();
+        assertFalse(result2);
     }
 
-    @org.junit.Test
+    @Test
     public void redo() throws Exception {
-
+        when(userDAO.createUser(any(User.class))).thenReturn(true);
+        boolean result = command.redo();
+        assertTrue(result);
+        when(userDAO.createUser(any(User.class))).thenReturn(false);
+        boolean result2 = command.redo();
+        assertFalse(result2);
     }
-
 }
